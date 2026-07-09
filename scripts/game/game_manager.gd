5c1418745c8a7dbf0bd24894ce978ab95283b562
Dziuras98 <dziuras98@gmail.com>
2026-07-09 21:33:06 +0200

    Refactor game manager to use race session controller

--- scripts/game/game_manager.gd
+++ scripts/game/game_manager.gd
@@ -3,6 +3,7 @@ extends Node3D
 const DEFAULT_CAR_CATALOG: CarCatalog = preload("res://resources/cars/catalog.tres")
 const CarSelectionState = preload("res://scripts/game/car_selection_state.gd")
 const MenuOptionsBuilder = preload("res://scripts/game/menu_options_builder.gd")
+const RaceSessionController = preload("res://scripts/game/race_session_controller.gd")
 
 @export_group("Cars")
 @export var car_catalog: CarCatalog = DEFAULT_CAR_CATALOG
@@ -27,10 +28,8 @@ var selected_mode_id: String = ""
 var selected_track_id: String = ""
 var selected_car_variant_id: StringName = &""
 var _car_selection_state: CarSelectionState
-var _opponents: Array[PlayerCarController] = []
-var _lap_tracker: LapTracker
 var _car_spawner: CarSpawner
-var _race_manager: RaceManager
+var _race_session: RaceSessionController
 var _race_hud: RaceHud
 var _mobile_drive_controls: CanvasLayer
 
@@ -52,9 +51,6 @@ func _ready() -> void:
 	_configure_menu_track_options()
 	_configure_menu_car_options()
 
-	_lap_tracker = LapTracker.new()
-	_lap_tracker.participant_finished.connect(_on_lap_tracker_participant_finished)
-
 	_car_spawner = CarSpawner.new()
 	_car_spawner.configure(
 		self,
@@ -69,13 +65,8 @@ func _ready() -> void:
 	_race_hud = RaceHud.new()
 	_build_mobile_drive_controls()
 
-	_race_manager = RaceManager.new()
-	_race_manager.countdown_changed.connect(_show_countdown)
-	_race_manager.countdown_hidden.connect(_hide_countdown)
-	_race_manager.player_input_enabled_changed.connect(_set_player_input_enabled)
-	_race_manager.ai_enabled_changed.connect(_set_ai_enabled)
-	_race_manager.opponent_should_stop.connect(_stop_participant_car)
-	_race_manager.race_finished.connect(_on_race_finished)
+	_race_session = RaceSessionController.new()
+	_race_session.configure(_car_spawner, _race_hud, _track, _minimap, race_lap_count, opponent_count)
 
 	if not _car_selection_state.has_available_options():
 		return
@@ -103,9 +94,8 @@ func _process(_delta: float) -> void:
 
 
 func _physics_process(_delta: float) -> void:
-	if _race_manager != null and _race_manager.should_update_race_positions() and _lap_tracker != null:
-		_lap_tracker.update_positions()
-		_update_lap_ui()
+	if _race_session != null:
+		_race_session.update_physics()
 
 
 func get_current_car() -> PlayerCarController:
@@ -113,16 +103,18 @@ func get_current_car() -> PlayerCarController:
 
 
 func get_opponents() -> Array[PlayerCarController]:
-	return _opponents
+	if _race_session == null:
+		var empty_opponents: Array[PlayerCarController] = []
+		return empty_opponents
+
+	return _race_session.get_opponents()
 
 
 func get_moving_opponent_count_for_test() -> int:
-	var moving_count: int = 0
-	for opponent: PlayerCarController in _opponents:
-		if is_instance_valid(opponent) and absf(opponent.get_forward_speed()) > 0.05:
-			moving_count += 1
+	if _race_session == null:
+		return 0
 
-	return moving_count
+	return _race_session.get_moving_opponent_count_for_test()
 
 
 func get_configured_opponent_count() -> int:
@@ -146,10 +138,10 @@ func request_return_to_main_menu_for_test() -> void:
 
 
 func simulate_current_player_finish_for_test() -> void:
-	if _current_car == null:
+	if _race_session == null:
 		return
 
-	_on_lap_tracker_participant_finished(_current_car)
+	_race_session.simulate_current_player_finish_for_test(_current_car)
 
 
 func is_child_visible_for_test(node_name: String) -> bool:
@@ -222,10 +214,8 @@ func _on_menu_selection_completed(mode_id: String, track_id: String, car_variant
 	selected_mode_id = mode_id
 	selected_track_id = track_id
 	_clear_current_car()
-	_clear_race_opponents()
-	_hide_countdown()
-	_hide_results()
-	_clear_race_tracking()
+	if _race_session != null:
+		_race_session.reset_to_menu_state()
 
 	var selected_car_index: int = _car_selection_state.get_car_index_for_variant_id(car_variant_id)
 	selected_car_variant_id = _car_selection_state.get_variant_id_for_index(selected_car_index)
@@ -234,7 +224,8 @@ func _on_menu_selection_completed(mode_id: String, track_id: String, car_variant
 	if selected_mode_id == MODE_RACE:
 		_start_race()
 	else:
-		_hide_lap_ui()
+		if _race_session != null:
+			_race_session.hide_lap_ui()
 
 
 func _spawn_car(car_index: int, spawn_transform: Transform3D) -> void:
@@ -247,9 +238,9 @@ func _spawn_car(car_index: int, spawn_transform: Transform3D) -> void:
 
 
 func _is_player_input_enabled_for_spawn() -> bool:
-	if _race_manager == null:
+	if _race_session == null:
 		return true
-	return not _race_manager.are_player_controls_locked()
+	return not _race_session.are_player_controls_locked()
 
 
 func _show_driving_ui_if_needed() -> void:
@@ -274,85 +265,13 @@ func _update_car_targets() -> void:
 			_minimap.call("set_target_node", _current_car)
 		if _track != null and _minimap.has_method("set_track_node"):
 			_minimap.call("set_track_node", _track)
-		if _minimap.has_method("set_opponents"):
-			_minimap.call("set_opponents", _opponents)
+		if _minimap.has_method("set_opponents") and _race_session != null:
+			_minimap.call("set_opponents", _race_session.get_opponents())
 
 
 func _start_race() -> void:
-	_spawn_opponents()
-	_prepare_race_tracking()
-	if _race_manager != null:
-		_race_manager.start_race(_current_car, get_tree())
-
-
-func _spawn_opponents() -> void:
-	if _car_spawner == null:
-		return
-
-	_opponents = _car_spawner.spawn_opponents(opponent_count)
-	_update_minimap_opponents()
-
-
-func _set_ai_enabled(enabled: bool) -> void:
-	if _car_spawner != null:
-		_car_spawner.set_ai_enabled(enabled)
-
-
-func _set_player_input_enabled(enabled: bool) -> void:
-	if _current_car != null:
-		_current_car.set_player_input_enabled(enabled)
-
-
-func _stop_participant_car(car: PlayerCarController) -> void:
-	if is_instance_valid(car):
-		car.set_external_drive_inputs(0.0, 0.85, 0.0)
-
-
-func _show_countdown(text: String) -> void:
-	if _race_hud != null:
-		_race_hud.show_countdown(text)
-
-
-func _hide_countdown() -> void:
-	if _race_hud != null:
-		_race_hud.hide_countdown()
-
-
-func _show_lap_ui() -> void:
-	if _race_hud != null:
-		_race_hud.show_lap()
-	_update_lap_ui()
-
-
-func _hide_lap_ui() -> void:
-	if _race_hud != null:
-		_race_hud.hide_lap()
-
-
-func _update_lap_ui() -> void:
-	if _current_car == null or _lap_tracker == null or _race_hud == null:
-		return
-
-	_race_hud.update_lap(
-		_lap_tracker.get_current_lap(_current_car),
-		maxi(race_lap_count, 1),
-		_lap_tracker.get_race_position(_current_car),
-		maxi(_lap_tracker.get_participant_count(), 1)
-	)
-
-
-func _hide_results() -> void:
-	if _race_hud != null:
-		_race_hud.hide_results()
-
-
-func _clear_race_opponents() -> void:
-	if _race_manager != null:
-		_race_manager.reset_to_idle()
-	if _car_spawner != null:
-		_car_spawner.clear_opponents()
-	_opponents.clear()
-	_update_minimap_opponents()
+	if _race_session != null:
+		_race_session.start_race(_current_car, get_tree())
 
 
 func _clear_current_car() -> void:
@@ -361,63 +280,10 @@ func _clear_current_car() -> void:
 	_current_car = null
 
 
-func _prepare_race_tracking() -> void:
-	if _lap_tracker == null:
-		return
-
-	_lap_tracker.prepare(_track, race_lap_count, _current_car, _opponents)
-	_show_lap_ui()
-
-
-func _clear_race_tracking() -> void:
-	if _lap_tracker != null:
-		_lap_tracker.clear()
-
-
-func _on_lap_tracker_participant_finished(car: PlayerCarController) -> void:
-	if car == _current_car:
-		_finish_race()
-	elif is_instance_valid(car):
-		_stop_participant_car(car)
-
-
-func _finish_race() -> void:
-	if _race_manager != null:
-		_race_manager.finish_race(_current_car, _opponents)
-
-
-func _on_race_finished() -> void:
-	_hide_lap_ui()
-	_show_results()
-
-
-func _show_results() -> void:
-	if _lap_tracker == null or _race_hud == null:
-		return
-
-	var result_labels: Array[String] = []
-	var ordered_participants: Array[PlayerCarController] = _lap_tracker.get_result_order()
-	for car: PlayerCarController in ordered_participants:
-		result_labels.append(_get_participant_label(car))
-
-	_race_hud.show_results(result_labels)
-
-
-func _get_participant_label(car: PlayerCarController) -> String:
-	if car == _current_car:
-		return "Ty"
-	if car != null and car.name != "":
-		return car.name
-	return "Kierowca"
-
-
 func _return_to_main_menu() -> void:
-	_hide_results()
-	_hide_lap_ui()
-	_hide_countdown()
-	_clear_race_opponents()
+	if _race_session != null:
+		_race_session.reset_to_menu_state()
 	_clear_current_car()
-	_clear_race_tracking()
 	selected_mode_id = ""
 	selected_track_id = ""
 	selected_car_variant_id = &""
@@ -427,17 +293,11 @@ func _return_to_main_menu() -> void:
 		_minimap.visible = false
 		if _minimap.has_method("set_target_node"):
 			_minimap.call("set_target_node", null)
-		_update_minimap_opponents()
 	if _menu != null:
 		if _menu.has_method("reset_menu"):
 			_menu.call("reset_menu")
 		else:
 			_menu.show()
-
-
-func _update_minimap_opponents() -> void:
-	if _minimap != null and _minimap.has_method("set_opponents"):
-		_minimap.call("set_opponents", _opponents)
 
 
 func _build_mobile_drive_controls() -> void:
---
+++ scripts/game/race_session_controller.gd
@@ -0,0 +1,223 @@
+extends RefCounted
+class_name RaceSessionController
+
+var _race_manager: RaceManager
+var _lap_tracker: LapTracker
+var _opponents: Array[PlayerCarController] = []
+var _current_car: PlayerCarController
+var _car_spawner: CarSpawner
+var _race_hud: RaceHud
+var _track: Node3D
+var _minimap: Node
+var _race_lap_count: int = 1
+var _opponent_count: int = 0
+
+
+func configure(
+	car_spawner: CarSpawner,
+	race_hud: RaceHud,
+	track: Node3D,
+	minimap: Node,
+	race_lap_count: int,
+	opponent_count: int
+) -> void:
+	_car_spawner = car_spawner
+	_race_hud = race_hud
+	_track = track
+	_minimap = minimap
+	_race_lap_count = maxi(race_lap_count, 1)
+	_opponent_count = maxi(opponent_count, 0)
+
+	_lap_tracker = LapTracker.new()
+	_lap_tracker.participant_finished.connect(_on_lap_tracker_participant_finished)
+
+	_race_manager = RaceManager.new()
+	_race_manager.countdown_changed.connect(_show_countdown)
+	_race_manager.countdown_hidden.connect(hide_countdown)
+	_race_manager.player_input_enabled_changed.connect(_set_player_input_enabled)
+	_race_manager.ai_enabled_changed.connect(_set_ai_enabled)
+	_race_manager.opponent_should_stop.connect(_stop_participant_car)
+	_race_manager.race_finished.connect(_on_race_finished)
+
+
+func start_race(current_car: PlayerCarController, scene_tree: SceneTree) -> void:
+	_current_car = current_car
+	_spawn_opponents()
+	_prepare_race_tracking()
+	if _race_manager != null:
+		_race_manager.start_race(_current_car, scene_tree)
+
+
+func update_physics() -> void:
+	if _race_manager != null and _race_manager.should_update_race_positions() and _lap_tracker != null:
+		_lap_tracker.update_positions()
+		_update_lap_ui()
+
+
+func reset_to_menu_state() -> void:
+	hide_results()
+	hide_lap_ui()
+	hide_countdown()
+	clear_opponents()
+	clear_tracking()
+
+
+func clear_opponents() -> void:
+	if _race_manager != null:
+		_race_manager.reset_to_idle()
+	if _car_spawner != null:
+		_car_spawner.clear_opponents()
+	_opponents.clear()
+	_update_minimap_opponents()
+
+
+func clear_tracking() -> void:
+	if _lap_tracker != null:
+		_lap_tracker.clear()
+
+
+func hide_countdown() -> void:
+	if _race_hud != null:
+		_race_hud.hide_countdown()
+
+
+func hide_lap_ui() -> void:
+	if _race_hud != null:
+		_race_hud.hide_lap()
+
+
+func hide_results() -> void:
+	if _race_hud != null:
+		_race_hud.hide_results()
+
+
+func get_opponents() -> Array[PlayerCarController]:
+	return _opponents
+
+
+func get_lap_tracker() -> LapTracker:
+	return _lap_tracker
+
+
+func get_race_manager() -> RaceManager:
+	return _race_manager
+
+
+func get_moving_opponent_count_for_test() -> int:
+	var moving_count: int = 0
+	for opponent: PlayerCarController in _opponents:
+		if is_instance_valid(opponent) and absf(opponent.get_forward_speed()) > 0.05:
+			moving_count += 1
+
+	return moving_count
+
+
+func simulate_current_player_finish_for_test(current_car: PlayerCarController) -> void:
+	if current_car == null:
+		return
+
+	_current_car = current_car
+	_on_lap_tracker_participant_finished(current_car)
+
+
+func are_player_controls_locked() -> bool:
+	if _race_manager == null:
+		return false
+
+	return _race_manager.are_player_controls_locked()
+
+
+func _spawn_opponents() -> void:
+	if _car_spawner == null:
+		return
+
+	_opponents = _car_spawner.spawn_opponents(_opponent_count)
+	_update_minimap_opponents()
+
+
+func _set_ai_enabled(enabled: bool) -> void:
+	if _car_spawner != null:
+		_car_spawner.set_ai_enabled(enabled)
+
+
+func _set_player_input_enabled(enabled: bool) -> void:
+	if _current_car != null:
+		_current_car.set_player_input_enabled(enabled)
+
+
+func _stop_participant_car(car: PlayerCarController) -> void:
+	if is_instance_valid(car):
+		car.set_external_drive_inputs(0.0, 0.85, 0.0)
+
+
+func _show_countdown(text: String) -> void:
+	if _race_hud != null:
+		_race_hud.show_countdown(text)
+
+
+func _show_lap_ui() -> void:
+	if _race_hud != null:
+		_race_hud.show_lap()
+	_update_lap_ui()
+
+
+func _update_lap_ui() -> void:
+	if _current_car == null or _lap_tracker == null or _race_hud == null:
+		return
+
+	_race_hud.update_lap(
+		_lap_tracker.get_current_lap(_current_car),
+		_race_lap_count,
+		_lap_tracker.get_race_position(_current_car),
+		maxi(_lap_tracker.get_participant_count(), 1)
+	)
+
+
+func _prepare_race_tracking() -> void:
+	if _lap_tracker == null:
+		return
+
+	_lap_tracker.prepare(_track, _race_lap_count, _current_car, _opponents)
+	_show_lap_ui()
+
+
+func _on_lap_tracker_participant_finished(car: PlayerCarController) -> void:
+	if car == _current_car:
+		_finish_race()
+	elif is_instance_valid(car):
+		_stop_participant_car(car)
+
+
+func _finish_race() -> void:
+	if _race_manager != null:
+		_race_manager.finish_race(_current_car, _opponents)
+
+
+func _on_race_finished() -> void:
+	hide_lap_ui()
+	_show_results()
+
+
+func _show_results() -> void:
+	if _lap_tracker == null or _race_hud == null:
+		return
+
+	var result_labels: Array[String] = []
+	var ordered_participants: Array[PlayerCarController] = _lap_tracker.get_result_order()
+	for car: PlayerCarController in ordered_participants:
+		result_labels.append(_get_participant_label(car))
+
+	_race_hud.show_results(result_labels)
+
+
+func _get_participant_label(car: PlayerCarController) -> String:
+	if car == _current_car:
+		return "Ty"
+	if car != null and car.name != "":
+		return car.name
+	return "Kierowca"
+
+
+func _update_minimap_opponents() -> void:
+	if _minimap != null and _minimap.has_method("set_opponents"):
+		_minimap.call("set_opponents", _opponents)
