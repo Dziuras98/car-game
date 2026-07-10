0bf0286aba2cea4678105d24a0188059b9f28566
Dziuras98 <64897952+Dziuras98@users.noreply.github.com>
2026-07-10 19:33:37 +0200

    Make the model own its explicit default variant

--- scripts/car/car_model_definition.gd
+++ scripts/car/car_model_definition.gd
@@ -11,6 +11,7 @@ class_name CarModelDefinition
 
 @export_group("Variants")
 @export var variants: Array[Resource] = []
+@export var default_variant_id: StringName = &""
 
 
 func get_model_name() -> String:
@@ -31,14 +32,17 @@ func get_variants() -> Array[CarVariantDefinition]:
 	return result
 
 
-func get_default_variant() -> CarVariantDefinition:
-	var available_variants: Array[CarVariantDefinition] = get_variants()
-	for variant: CarVariantDefinition in available_variants:
-		if variant.is_default:
+func get_variant_by_id(variant_id: StringName) -> CarVariantDefinition:
+	for variant: CarVariantDefinition in get_variants():
+		if variant.variant_id == variant_id:
 			return variant
-	if not available_variants.is_empty():
-		return available_variants[0]
 	return null
+
+
+func get_default_variant() -> CarVariantDefinition:
+	if default_variant_id == &"":
+		return null
+	return get_variant_by_id(default_variant_id)
 
 
 func get_variant_count() -> int:
