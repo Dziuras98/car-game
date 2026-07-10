52bb8dc16d5ae28d4f08ec6e586627fb6bc88a4f
Dziuras98 <64897952+Dziuras98@users.noreply.github.com>
2026-07-10 14:48:10 +0200

    Add runtime track spawn controller

---
+++ scripts/game/track_spawn_controller.gd
@@ -0,0 +1,45 @@
+extends RefCounted
+class_name TrackSpawnController
+
+var _container: Node3D
+var _current_track: Node3D
+var _current_definition: TrackDefinition
+
+
+func configure(container: Node3D) -> void:
+	_container = container
+
+
+func get_current_track() -> Node3D:
+	return _current_track
+
+
+func get_current_definition() -> TrackDefinition:
+	return _current_definition
+
+
+func spawn_track(definition: TrackDefinition) -> Node3D:
+	if _container == null or definition == null or not definition.is_valid():
+		return null
+
+	clear_track()
+	var track: Node3D = definition.instantiate_track()
+	if track == null:
+		push_error("Track definition '%s' did not instantiate a Node3D root." % str(definition.track_id))
+		return null
+
+	track.name = "ActiveTrack"
+	_container.add_child(track)
+	_current_track = track
+	_current_definition = definition
+	return track
+
+
+func clear_track() -> void:
+	if is_instance_valid(_current_track):
+		var parent: Node = _current_track.get_parent()
+		if parent != null:
+			parent.remove_child(_current_track)
+		_current_track.queue_free()
+	_current_track = null
+	_current_definition = null
