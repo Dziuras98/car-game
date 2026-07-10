62e92ec2811d59edc9607dc16ab6017027c355a6
Dziuras98 <64897952+Dziuras98@users.noreply.github.com>
2026-07-10 17:18:33 +0200

    Load translation catalogs explicitly at runtime

---
+++ scripts/ui/localization_catalog_loader.gd
@@ -0,0 +1,32 @@
+extends RefCounted
+class_name LocalizationCatalogLoader
+
+const CATALOG_PATHS: PackedStringArray = PackedStringArray([
+	"res://translations/pl.po",
+	"res://translations/en.po",
+])
+
+static var _catalogs_registered: bool = false
+
+
+static func ensure_loaded() -> PackedStringArray:
+	var errors: PackedStringArray = PackedStringArray()
+	if _catalogs_registered:
+		return errors
+
+	for catalog_path: String in CATALOG_PATHS:
+		if not ResourceLoader.exists(catalog_path, "Translation"):
+			errors.append("translation catalog does not exist: %s" % catalog_path)
+			continue
+		var catalog: Resource = ResourceLoader.load(catalog_path, "Translation")
+		if not catalog is Translation:
+			errors.append("translation catalog did not load as Translation: %s" % catalog_path)
+			continue
+		TranslationServer.add_translation(catalog as Translation)
+
+	_catalogs_registered = errors.is_empty()
+	return errors
+
+
+static func get_catalog_paths() -> PackedStringArray:
+	return CATALOG_PATHS.duplicate()
