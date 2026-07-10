de229c3021d729d14d20edf4b9c15c31e7c622f7
Dziuras98 <64897952+Dziuras98@users.noreply.github.com>
2026-07-10 19:08:18 +0200

    Remove per-track default flag

--- scripts/track/track_definition.gd
+++ scripts/track/track_definition.gd
@@ -4,7 +4,6 @@ class_name TrackDefinition
 @export_group("Identity")
 @export var track_id: StringName = &""
 @export var display_name: String = ""
-@export var is_default: bool = false
 @export_range(1, 99, 1) var recommended_laps: int = 3
 
 @export_group("Runtime")
