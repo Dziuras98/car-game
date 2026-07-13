4c0acf7b98f23693ae2da81c95eb9da28205df2f
Dziuras98 <64897952+Dziuras98@users.noreply.github.com>
2026-07-13 11:24:19 +0200

    Construct pit barrier ranges at runtime

--- scripts/tracks/tor_poznan_barrier_configuration.gd
+++ scripts/tracks/tor_poznan_barrier_configuration.gd
@@ -2,17 +2,17 @@ extends Node
 class_name TorPoznanBarrierConfiguration
 
 const RIGHT_BARRIER_EXCLUSION_META: StringName = &"right_barrier_exclusion_ranges"
-const PIT_LANE_EXCLUSION_RANGES: PackedVector2Array = PackedVector2Array([
-	Vector2(0.0, 0.065),
-	Vector2(0.945, 1.0),
-])
 
 
 func _enter_tree() -> void:
 	var track: GeneratedTrack = get_parent() as GeneratedTrack
 	if track == null or track.track_layout == null:
 		return
+	var exclusion_ranges: PackedVector2Array = PackedVector2Array([
+		Vector2(0.0, 0.065),
+		Vector2(0.945, 1.0),
+	])
 	track.track_layout.set_meta(
 		RIGHT_BARRIER_EXCLUSION_META,
-		PIT_LANE_EXCLUSION_RANGES
+		exclusion_ranges
 	)
